b9e936cef91e1c736eb196e5e3fe6976c635d3e1
Kyle <kyle.simon.tong@gmail.com>
2020-03-04 21:28:09 +1300

    Get the token working with local storage and handling its own refreshing

--- app/services/token.js
+++ app/services/token.js
@@ -7,6 +7,17 @@ export default class TokenService extends Service {
   @tracked token;
 
   get currentActiveToken() {
+    const tokenDetails = JSON.parse(localStorage.getItem('tokenDetails'));
+    const now = new Date();
+    
+    if (tokenDetails && tokenDetails.expiry > now.toISOString()) {
+      this.token = tokenDetails.token;
+    } else {
+      return this.requestNewToken();
+    }
+  }
+
+  requestNewToken() {
     return fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
       headers: {
@@ -15,8 +26,17 @@ export default class TokenService extends Service {
       },
       body: 'grant_type=client_credentials',
     }).then(r => r.json())
-        .then(response => {
-          this.token = response.access_token;
+      .then(response => {
+        // Set the request time to be the time we get the response back.
+        const now = new Date();
+        now.setSeconds(now.getSeconds() + response.expires_in);
+        const expiry = now;
+
+        localStorage.setItem('tokenDetails', JSON.stringify({
+          token: response.access_token,
+          expiry,
+        }));
+        this.token = response.access_token;
     });
   }
 }
