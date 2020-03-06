e3f2bc54b75216e15a6cd919096e17e806da7e5f
Kyle <kyle.simon.tong@gmail.com>
2020-03-06 21:11:20 +1300

    Get search bar working with ember concurrency and start trying to pass the data to a results component, but maybe we don't need a separate component

--- app/components/search-bar/search-bar.js
+++ app/components/search-bar/search-bar.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from "@ember/object";
 import { tracked } from '@glimmer/tracking';
+import { timeout } from 'ember-concurrency';
 import { task } from 'ember-concurrency-decorators';
 import fetch from 'fetch';
 
@@ -12,23 +13,36 @@ export default class SearchBarComponent extends Component {
 
     @tracked results;
 
-    @task({ restartable: true, maxConcurrency: 1 })
-    searchTask = function* () {
+    @action
+    keyUp() {
+        this.searchTask.perform(this.input);
     }
 
-    async search() {
-        const urlEncodedInput = encodeURIComponent(this.input);
+    @action updateResults() {
+        
+    }
+
+    @task({ restartable: true, maxConcurrency: 1 })
+    searchTask = function* (input) {
+        const urlEncodedInput = encodeURIComponent(input);
         // TODO: Move url to env file
         const url = `https://api.spotify.com/v1/search?type=artist&q=${urlEncodedInput}`;
 
         if (!urlEncodedInput) {
             return;
         }
-        
+
+        yield timeout(150);
+
+        this.results = yield this.search(url);
+    }
+
+    async search(url) {
         const { headers } = this.store.adapterFor('application');
 
         const response = await fetch(url, { headers });
+        const results = await response.json();
 
-        this.results = await response.json();
+        return results;
     }
 }
