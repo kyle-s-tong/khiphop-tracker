beb5bda93a2c6d9a7bc697f35ededfcdfef655d8
Kyle <kyle.simon.tong@gmail.com>
2020-04-04 11:55:04 +1300

    Make the home page also use the new artist album component

--- app/components/artist-album/artist-album.js
+++ app/components/artist-album/artist-album.js
@@ -1,4 +1,9 @@
 import Component from '@glimmer/component';
+import { get } from '@ember/object';
 
 export default class ArtistAlbumComponent extends Component {
+  get albumArtistDifferentFromArtist() {
+    const albumArtistId = get(this.args.album.artist, 'id');
+    return this.args.artist.id !== albumArtistId;
+  }
 }
