7a317af4cc4baf48b30660a22ccde4ef8557dffd
Kyle <kyle@realestate.co.nz>
2020-04-14 08:01:42 +1200

    [WIP] Add new way of grabbing the latest releases from an artists

--- app/routes/new-releases.js
+++ app/routes/new-releases.js
@@ -1,21 +1,19 @@
 import Route from '@ember/routing/route';
-import Moment from 'moment';
 
 export default class NewReleasesRoute extends Route {
   async model() {
     const artists = await this.store.findAll('artist');
 
     const artistsNewAlbums = await Promise.all(artists.map(async (artist) => {
-      const albums = await artist.albums;
-
-      const newReleases = albums.filter(album => {
-        const now = new Moment();
-        return album.release_date > now.subtract(7, 'days').format('YYYY-MM-DD');
+      const latestReleases = await this.store.query('album', {
+        filter: {
+          latestReleases: true
+        }
       })
 
       return {
         artist,
-        albums: newReleases,
+        albums: latestReleases,
       }
     }));
 
