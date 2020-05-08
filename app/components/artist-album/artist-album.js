ed68270d5abde14de1cb8d11776d1bce87134857
Kyle <kyle.simon.tong@gmail.com>
2020-05-09 11:41:13 +1200

    Fix the filter for when the artist doesnt match the owner of the album

--- app/components/artist-album/artist-album.js
+++ app/components/artist-album/artist-album.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 
 export default class ArtistAlbumComponent extends Component {
   get albumArtistDifferentFromArtist() {
-    return !this.args.album.albumArtists.filter(artist => artist.id === this.args.artist.id);
+    const filteredAlbumArtists = this.args.album.albumArtists.filter(artist => artist.id === this.args.artist.id)
+    return filteredAlbumArtists.length === 0;
   }
 }
