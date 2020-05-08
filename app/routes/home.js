e5649077dc8e2f5cd78eddb1186f78289dd82f7c
Kyle <kyle.simon.tong@gmail.com>
2020-05-09 11:45:42 +1200

    Unload artists on home page to make sure we don't carry any over from the release page

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class HomeRoute extends Route {
+    beforeModel() {
+      // Sometimes we get extra artists in the store from the new releases page, so unload
+      // all of these before we do another request to the API.
+      this.store.unloadAll('artist');
+    }
+
     async model() {
-      return this.store.findAll('artist');
+      return this.store.findAll('artist', { reload: true });
     }
 }
