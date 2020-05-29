f3ec5770da5d525ee50c4dbc2d01df7a7d55d007
Kyle <kyle.simon.tong@gmail.com>
2020-05-30 10:13:27 +1200

    Change some styles to help with mobile view and unload albums before loading the release page to stop data contamination from the tracked page

--- app/routes/new-releases.js
+++ app/routes/new-releases.js
@@ -1,6 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class NewReleasesRoute extends Route {
+  beforeModel() {
+      // Sometimes we get extra albums in the store from the tracked artists page, so unload
+      // all of these before we do another request to the API.
+      this.store.unloadAll('album');
+  }
+
   async model() {
     const artists = await this.store.findAll('artist');
 
